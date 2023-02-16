d8504737a926a8c5bcfab86389baa21de5bab20b
agusosilva <agusosilva@hotmail.com>
2023-02-15 22:17:09 -0300

    Introduciendo bases para iniciar/fuentes/colores - Tambien inicio del Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,28 @@ module.exports = {
     "./*.html"
   ],
   theme: {
-    extend: {},
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '10rem',
+      },
+    },
+    extend: {
+      colors:{
+        'back-black':'#16161A',
+        'borders-black':'#4B4F55',
+        'back-black-medium':'#2C2D32',
+
+      },
+      fontFamily:{
+        'ME Regular':"'Monument Extended Regular', sans-serif",
+        'ME UltraBold':"'Monument Extended UltraBold', sans-serif",
+        'Inter':"'Inter', sans-serif",
+    }
+    },
   },
   plugins: [],
 }
